129b756fce7bf693f98b81b0f7bd5bcec9bf1c9b
Sachin Sharma <sachin.sharma@vgroup.net>
2020-10-21 01:06:57 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { StatesComponent } from './states/states.component';
+import { TopicsComponent } from './topics/topics.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'states', component: StatesComponent },
+  { path: 'topics', component: TopicsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import * as jQuery from 'jquery';// import Jquery here    
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'stately';
+
+  ngOnInit(){
+   jQuery('.icon').click(function () {
+    jQuery('.search').toggleClass('expanded');
+  });
+  }
 }
+
+
